802b7cb51aece2f6d693f48c1459d480180e6d4a
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-21 15:51:01 +0200

    Remove source comments and DocC

--- Sources/Memory Shared Primitives/Memory.Shared.Error.swift
+++ Sources/Memory Shared Primitives/Memory.Shared.Error.swift
@@ -1,34 +1,19 @@
-// ===----------------------------------------------------------------------===//
-//
-// This source file is part of the swift-memory-shared-primitives open source project
-//
-// Copyright (c) 2024-2026 Coen ten Thije Boonkkamp and the swift-memory-shared-primitives project authors
-// Licensed under Apache License v2.0
-//
-// See LICENSE for license information
-//
-// ===----------------------------------------------------------------------===//
-
 public import Error_Primitives
 
 extension Memory.Shared {
-    /// Errors from shared memory syscalls.
+
     public enum Error: Swift.Error, Sendable, Equatable {
-        /// shm_open failed.
+
         case open(Error_Primitives.Error.Code)
 
-        /// shm_unlink failed.
         case unlink(Error_Primitives.Error.Code)
 
-        /// Out of memory (`ENOMEM`).
         case exhausted
     }
 }
 
-// MARK: - CustomStringConvertible
-
 extension Memory.Shared.Error: CustomStringConvertible {
-    /// A human-readable description of the shared-memory failure.
+
     public var description: Swift.String {
         switch self {
         case .open(let code):
--- Sources/Memory Shared Primitives/exports.swift
+++ Sources/Memory Shared Primitives/exports.swift
@@ -1,9 +1,2 @@
-//
-//  exports.swift
-//  swift-memory-shared-primitives
-//
-//  Created by Coen ten Thije Boonkkamp on 20/05/2026.
-//
-
 @_exported public import Error_Primitives
 @_exported public import Memory_Primitive
